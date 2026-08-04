edf4cc0b771a13b181fe71c387deabc996e21cf5
pchmirenko <developer31f@gmail.com>
2026-08-04 17:40:23 +0200

    test: make calibration routing deterministic

--- tests/calibration-e2e-integration.test.js
+++ tests/calibration-e2e-integration.test.js
@@ -144,6 +144,7 @@ function run() {
                 policyPath,
                 '--category',
                 'coding',
+                '--no-registry',
                 '--no-verbose'
             ],
             tempDir,
